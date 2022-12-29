c77fdd4f8a0e8db22b84f3207908bb8040e6d598
t13rm4in <119757490+t13rm4in@users.noreply.github.com>
2022-12-29 19:34:24 +0300

    Добавьте файлы проекта.

---
+++ ЛР3 (Логические выражения)/ЛР3 (Логические выражения).cpp
@@ -0,0 +1,19 @@
+﻿# include <iostream>
+# include <windows.h>
+# include <cmath>
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+	int A, B;
+	bool answer;
+	cout << "Введите число A: ";
+	cin >> A;
+	cout << "Введите число B: ";
+	cin >> B;
+	answer = A % 2 == 1 && B % 2 == 0 || A % 2 == 0 && B % 2 == 1;
+	answer = (A % 2 + B % 2) == 1;
+	cout << "Только одно из чисел A и B нечетное: " << boolalpha << answer << endl;
+	return 0;
+}
